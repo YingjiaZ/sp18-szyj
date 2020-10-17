57819c14340e6d5103757b31e8024aad8ba3bc61
YingjiaZ <szhangyingjia@gmail.com>
2020-10-17 18:36:59 +0800

    proj1gold corrected

--- proj1gold/TestArrayDequeGold.java
+++ proj1gold/TestArrayDequeGold.java
@@ -6,7 +6,7 @@ public class TestArrayDequeGold {
     public void testGold() {
         StudentArrayDeque<Integer> stu = new StudentArrayDeque<>();
         ArrayDequeSolution<Integer> slu = new ArrayDequeSolution<>();
-        String message = null;
+        String message = "\n";
         int size = 0;
 
         for (int i = 0; i < 20; i += 1) {
@@ -16,25 +16,23 @@ public class TestArrayDequeGold {
                 stu.addLast(i);
                 slu.addLast(i);
                 size += 1;
-                int randomIndex = StdRandom.uniform(size);
-                assertEquals(stu.get(randomIndex),slu.get(randomIndex));
                 message = message + "addLast(" + i + ")" + "\n";
+                assertEquals(message,slu.get(size - 1),stu.get(size - 1));
             } else if (numberBetweenZeroAndOne > 0.25 && numberBetweenZeroAndOne < 0.5) {
                 stu.addFirst(i);
                 slu.addFirst(i);
                 size += 1;
-                int randomIndex = StdRandom.uniform(size);
-                assertEquals(stu.get(randomIndex),slu.get(randomIndex));
                 message = message + "addFirst(" + i + ")" + "\n";
+                assertEquals(message,slu.get(0),stu.get(0));
             } else if (numberBetweenZeroAndOne > 0.5 && numberBetweenZeroAndOne < 0.75) {
                 if (size == 0) {
                     continue;
                 } else {
-                    Integer sturl = stu.removeFirst();
+                    Integer sturl = stu.removeLast();
                     Integer slurl = slu.removeLast();
                     message = message + "removeLast()" + "\n";
                     size -= 1;
-                    assertEquals(message, sturl, slurl);
+                    assertEquals(message, slurl, sturl);
                 }
             } else if (numberBetweenZeroAndOne > 0.75) {
                 if (size == 0) {
@@ -44,7 +42,7 @@ public class TestArrayDequeGold {
                     Integer slurf = slu.removeFirst();
                     message = message + "removeFirst()" + "\n";
                     size -= 1;
-                    assertEquals(message, sturf, slurf);
+                    assertEquals(message, slurf, sturf);
 
                 }
             }

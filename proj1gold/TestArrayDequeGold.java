6b7a36279fc81781b1748b331e1d3018557b70fe
YingjiaZ <szhangyingjia@gmail.com>
2020-10-17 19:02:19 +0800

    proj1 gold modified

--- proj1gold/TestArrayDequeGold.java
+++ proj1gold/TestArrayDequeGold.java
@@ -6,25 +6,25 @@ public class TestArrayDequeGold {
     public void testGold() {
         StudentArrayDeque<Integer> stu = new StudentArrayDeque<>();
         ArrayDequeSolution<Integer> slu = new ArrayDequeSolution<>();
-        String message = "\n";
+        String message = "";
         int size = 0;
 
-        for (int i = 0; i < 20; i += 1) {
+        for (int i = 0; i < 100; i += 1) {
             double numberBetweenZeroAndOne = StdRandom.uniform();
 
-            if (numberBetweenZeroAndOne < 0.25) {
+            if (numberBetweenZeroAndOne <= 0.25) {
                 stu.addLast(i);
                 slu.addLast(i);
                 size += 1;
                 message = message + "addLast(" + i + ")" + "\n";
                 assertEquals(message,slu.get(size - 1),stu.get(size - 1));
-            } else if (numberBetweenZeroAndOne > 0.25 && numberBetweenZeroAndOne < 0.5) {
+            } else if (numberBetweenZeroAndOne > 0.25 && numberBetweenZeroAndOne <= 0.5) {
                 stu.addFirst(i);
                 slu.addFirst(i);
                 size += 1;
                 message = message + "addFirst(" + i + ")" + "\n";
                 assertEquals(message,slu.get(0),stu.get(0));
-            } else if (numberBetweenZeroAndOne > 0.5 && numberBetweenZeroAndOne < 0.75) {
+            } else if (numberBetweenZeroAndOne > 0.5 && numberBetweenZeroAndOne <= 0.75) {
                 if (size == 0) {
                     continue;
                 } else {
